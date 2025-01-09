dec61c93cacfdcccb9f3b234a4d8fdb6bfa2682b
Mahima Kumari <mahimayadowansi@gmail.com>
2025-01-09 13:47:20 +0530

    NEW UPDATE

---
+++ src/test/java/customListerners/Practice.java
@@ -0,0 +1,13 @@
+package customListerners;
+
+import org.testng.annotations.Test;
+
+public class Practice {
+	@Test()
+	public void add()
+	{
+		int a=4,b=10;
+		System.out.println("The sum is: "+(a+b));
+	}
+
+}
